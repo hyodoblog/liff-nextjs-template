5920aa03e2b7bec5c63a64f3d191d8c45d605601
hyodoblog <yusei.hyodo@gmail.com>
2022-05-22 19:42:18 +0900

    トップページの表示調整

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,10 +9,21 @@ const HomePage: NextPage = () => {
 
   return (
     <DefaultLayout>
-      <div style={{ marginTop: '20%', textAlign: 'center' }}>
-        name: {user!.name}
-        <br />
-        userUid: {user!.userUid}
+      <div style={{ marginTop: '20%' }}>
+        <h1 style={{ fontSize: '2rem', marginBottom: 8, textAlign: 'center' }}>ようこそ、LIFFの世界へ</h1>
+
+        <table style={{ margin: 'auto' }}>
+          <tbody>
+            <tr>
+              <td>LINE表示名</td>
+              <td>：{user!.name}</td>
+            </tr>
+            <tr>
+              <td>userUid</td>
+              <td>：{user!.userUid}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </DefaultLayout>
   )
