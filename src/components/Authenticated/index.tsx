d3ecd80e564904f0960a3fb9feac41e5d6975402
hyodoblog <yusei.hyodo@gmail.com>
2022-10-02 14:58:28 +0900

    🚑 authenticated component.

--- src/components/Authenticated/index.tsx
+++ src/components/Authenticated/index.tsx
@@ -1,42 +1,59 @@
 import { LiffMockPlugin } from '@line/liff-mock'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import Script from 'next/script'
 import { useContext } from 'react'
 
 import { AuthContext } from '~/contexts/AuthContext'
 
 const liffId = process.env.NEXT_PUBLIC_LIFF_ID!
+// const [lineChannelId, _] = liffId.split('-')
 
 export const Authenticated = () => {
+  const auth = getAuth()
   const { setUser: setUserContext } = useContext(AuthContext)
 
-  const setUser = async (userUid: string): Promise<void> => {
-    // 今回はデモ用のテンプレートコードなので、nameに仮のdisplayNameを設定している
-    // 本来はここでuserUidをもとにDBから値を取り、setUserContextに反映させる
-    setUserContext({
-      userUid,
-      name: (await liff.getProfile()).displayName
-    })
+  const login = async (): Promise<void> => {
+    const idToken = liff.getIDToken()!
+    console.info(idToken, 'をもとにfirebase tokenを取得する')
+    // const { token } = await apiLogin({ idToken, channelId: lineChannelId })
+    // await signInWithCustomToken(auth, token)
   }
 
-  const handleError = (err: any) => {
-    console.error(err)
-    setUserContext(null)
+  const setUser = async (userId: string): Promise<void> => {
+    // setUserContext(user)
   }
 
   const liffInit = async () => {
+    const handleError = (err: any) => {
+      console.error(err)
+      setUserContext(null)
+    }
+
     try {
       if (process.env.NODE_ENV === 'development') {
         liff.use(new LiffMockPlugin())
+        // @ts-ignore
         await liff.init({ liffId, mock: true })
         liff.login()
       } else {
         await liff.init({ liffId })
       }
 
-      const profile = await liff.getProfile()
-      setUser(profile.userId)
+      await login()
 
-      console.info(profile)
+      onAuthStateChanged(
+        auth,
+        async (user) => {
+          try {
+            if (user) {
+              await setUser(user.uid)
+            }
+          } catch (err) {
+            handleError(err)
+          }
+        },
+        (err) => handleError(err)
+      )
     } catch (err) {
       handleError(err)
     }
